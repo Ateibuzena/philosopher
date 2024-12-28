d3c43df978500bc7ce79ae1b7cc3cfd62724e303
Ana Zubieta Pena <azubieta@c3r7s2.42malaga.com>
2024-12-28 14:52:50 +0100

    modificacion de logica internat para crear ejecucion de ciclos a la vez

--- src/philo_lifecycle.c
+++ src/philo_lifecycle.c
@@ -6,51 +6,45 @@
 /*   By: azubieta <azubieta@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 21:02:44 by azubieta          #+#    #+#             */
-/*   Updated: 2024/12/27 15:28:13 by azubieta         ###   ########.fr       */
+/*   Updated: 2024/12/27 20:22:46 by azubieta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philoft.h"
 
-static int	ft_sleeping(t_philo *philo)
+static void	ft_sleeping(t_philo *philo)
 {
 	if (ft_simulation_lock(philo->env))
-		return (0);
+		return ;
 	ft_print("is sleeping", philo->env, philo->id);
-	ft_msleep(philo->env->time_to_sleep);
-	return (1);
+	usleep(philo->env->time_to_sleep * 1000);
 }
 
 static void	ft_forks_ids(t_philo *philo, long int *first, long int *second)
 {
 	if (philo->id % 2 == 0)
 	{
-		*first = philo->right_fork;
-		*second = philo->left_fork;
-	}
-	else
-	{
 		*first = philo->left_fork;
 		*second = philo->right_fork;
 	}
+	else
+	{
+		*first = philo->right_fork;
+		*second = philo->left_fork;
+	}
 }
 
-static int	ft_take_forks(t_philo *philo)
+static int	ft_take_forks(t_philo *philo, long int *first, long int *second)
 {
-	long int	first_fork;
-	long int	second_fork;
-
-	first_fork = -1;
-	second_fork = -1;
 	if (ft_simulation_lock(philo->env))
 		return (0);
-	ft_forks_ids(philo, &first_fork, &second_fork);
-	pthread_mutex_lock(&philo->env->forks[first_fork]);
+	ft_forks_ids(philo, first, second);
+	pthread_mutex_lock(&philo->env->forks[*first]);
 	ft_print("has taken a fork", philo->env, philo->id);
 	if ((philo->env->num_philos == 1)
-		|| (pthread_mutex_lock(&philo->env->forks[second_fork]) != 0))
+		|| (pthread_mutex_lock(&philo->env->forks[*second]) != 0))
 	{
-		pthread_mutex_unlock(&philo->env->forks[first_fork]);
+		pthread_mutex_unlock(&philo->env->forks[*first]);
 		return (0);
 	}
 	ft_print("has taken a fork", philo->env, philo->id);
@@ -62,17 +56,18 @@ static int	ft_eating(t_philo *philo)
 	long int	first_fork;
 	long int	second_fork;
 
-	if (!ft_take_forks(philo))
-		return (0);
 	first_fork = -1;
 	second_fork = -1;
-	ft_forks_ids(philo, &first_fork, &second_fork);
+	if (ft_simulation_lock(philo->env))
+		return (0);
+	if (!ft_take_forks(philo, &first_fork, &second_fork))
+		return (0);
 	ft_print("is eating", philo->env, philo->id);
 	pthread_mutex_lock(&philo->env->simulation_lock);
 	philo->last_meal_time = ft_get_time() - philo->env->start_time;
 	philo->meals_eaten++;
-	ft_msleep(philo->env->time_to_eat);
 	pthread_mutex_unlock(&philo->env->simulation_lock);
+	usleep(philo->env->time_to_eat * 1000);
 	pthread_mutex_unlock(&philo->env->forks[second_fork]);
 	pthread_mutex_unlock(&philo->env->forks[first_fork]);
 	return (1);
@@ -84,16 +79,18 @@ void	*ft_lifecycle(void *arg)
 
 	philo = (t_philo *)arg;
 	if (philo->id % 2 != 0)
-		usleep(200);
+		usleep(500);
 	while (1)
 	{
 		if (!ft_eating(philo))
-			return (NULL);
-		else
-			ft_sleeping(philo);
+			break ;
 		if (ft_simulation_lock(philo->env))
-			return (NULL);
+			break ;
+		ft_sleeping(philo);
+		if (ft_simulation_lock(philo->env))
+			break ;
 		ft_print("is thinking", philo->env, philo->id);
+		usleep(500);
 	}
 	return (NULL);
 }
